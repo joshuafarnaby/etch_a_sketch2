ee6ae0b7f83ef837b460618aa25c7184a0d1ed9e
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-12-01 14:25:16 +0000

    add reset functionality to main-container when reset button is clicked

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mainContainer = document.getElementById('main-container');
+const resetBtn = document.getElementById('reset-button');
 
 
 // declared here so the values can be accessed below
@@ -20,7 +21,26 @@ mainContainer.childNodes.forEach(node => {
   })
 })
 
+resetBtn.addEventListener('click', () => {
+  while (mainContainer.firstChild) {
+    mainContainer.removeChild(mainContainer.firstChild)
+  }
 
+  for (let i = 1; i <= 16; i++) {
+    let newDiv = document.createElement('div');
+  
+    newDiv.style.width = `${500 / 4}px`;
+    newDiv.style.height = `${500 / 4}px`;
+  
+    mainContainer.appendChild(newDiv);
+  }
+
+  mainContainer.childNodes.forEach(node => {
+    node.addEventListener('mouseover', (e) => {
+      e.target.style.backgroundColor = '#333';
+    })
+  })
+})
 
 
 
