834fecaed108e8fa8755f3476fb016026cd2ebfe
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-12-01 13:51:08 +0000

    populate main container with 16 divs in a 4x4 grid

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const mainContainer = document.getElementById('main-container');
 mainContainer.style.height = '500px';
 mainContainer.style.width = '500px';
 mainContainer.style.border = '1px solid #111';
+mainContainer.style.display = 'flex';
+mainContainer.style.flexFlow = 'row wrap';
+
+for (let i = 1; i <= 16; i++) {
+  let newDiv = document.createElement('div');
+
+  newDiv.style.width = `${500 / 4}px`;
+  newDiv.style.height = `${500 / 4}px`;
+
+  mainContainer.appendChild(newDiv);
+}
 
 
 
@@ -15,53 +26,3 @@ mainContainer.style.border = '1px solid #111';
 
 
 
-
-
-
-// const container = document.getElementById('main-container');
-// // const containerWidth = container.offsetWidth;
-
-// // container.style.width = '500px';
-// // container.style.height = '500px';
-
-
-
-// for (let i = 1; i <= 1296; i++) {
-//   let div = document.createElement('div');
-
-//   div.classList = 'test'
-
-//   // div.classList.add('div-border')
-//   div.style.height = `${500 / 36}px`;
-//   div.style.width = `${500 / 36}px`;
-
-//   container.appendChild(div)
-// }
-
-// console.log(container.childNodes);
-
-
-
-// container.childNodes.forEach(node => {
-//   node.addEventListener('mouseover', (e) =>{
-  
-
-//     if (spacePressed() && e.target.className == 'test') {
-//       e.target.style.backgroundColor = 'white';
-//     } else {
-//       e.target.style.backgroundColor = 'red'
-//     }
-    
-//   })
-// })    
-
-// function spacePressed() {
-//   document.body.onkeydown = function(e) {
-//       if (e.code == 'Space') {
-//         console.log('space down');
-//         return true;
-//       } else {
-//         return false
-//       }
-//     }
-// }
