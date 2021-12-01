5feeb8d8aeaf6a07cd0b5789fcaabd6ba88a8a3b
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-12-01 13:34:56 +0000

    add style to main-container on page load; 500px a side and solid border

---
+++ script.js
@@ -0,0 +1,67 @@
+const mainContainer = document.getElementById('main-container');
+
+mainContainer.style.height = '500px';
+mainContainer.style.width = '500px';
+mainContainer.style.border = '1px solid #111';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const container = document.getElementById('main-container');
+// // const containerWidth = container.offsetWidth;
+
+// // container.style.width = '500px';
+// // container.style.height = '500px';
+
+
+
+// for (let i = 1; i <= 1296; i++) {
+//   let div = document.createElement('div');
+
+//   div.classList = 'test'
+
+//   // div.classList.add('div-border')
+//   div.style.height = `${500 / 36}px`;
+//   div.style.width = `${500 / 36}px`;
+
+//   container.appendChild(div)
+// }
+
+// console.log(container.childNodes);
+
+
+
+// container.childNodes.forEach(node => {
+//   node.addEventListener('mouseover', (e) =>{
+  
+
+//     if (spacePressed() && e.target.className == 'test') {
+//       e.target.style.backgroundColor = 'white';
+//     } else {
+//       e.target.style.backgroundColor = 'red'
+//     }
+    
+//   })
+// })    
+
+// function spacePressed() {
+//   document.body.onkeydown = function(e) {
+//       if (e.code == 'Space') {
+//         console.log('space down');
+//         return true;
+//       } else {
+//         return false
+//       }
+//     }
+// }
